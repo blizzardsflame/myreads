bd770d4c70c8c620e19faf82b9964f980f3cf0ed
BlizzardsFlame <ned.r@hotmail.fr>
2020-04-08 05:37:44 +0200

    feat: Splitting search component
    Splitting the search component into search bar and results.
    The Ui is completely split at this point.

--- src/App.js
+++ src/App.js
@@ -114,31 +114,47 @@ class BookSearch extends Component{
     render(){
         return(
             <div className="search-books">
-                <div className="search-books-bar">
-                    <button
-                        className="close-search"
-                        onClick={() => this.setState({ showSearchPage: false })}
-                    >
-                        Close
-                    </button>
-                    <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author" />
-                    </div>
-                </div>
-                <div className="search-books-results">
-                    <ol className="books-grid" />
-                </div>
+                <SearchBar />
+                <Searchresults />
             </div>
-        )
+        );
     }
 }
+
+const SearchBar = props => {
+    return(
+        <div className="search-books-bar">
+            <CloseButton />
+            <SearchInput />
+        </div>
+    );
+};
+
+const CloseButton = () => {
+    return(
+        <Link to="/">
+        <button className="close-search">Close</button>
+        </Link>
+    );
+};
+
+class SearchInput extends Component{
+    render() {
+        return(
+            <div className="search-books-input-wrapper">
+                <input type="text" placeholder="Search by title or author" />
+            </div>
+        );
+}}
+
+const Searchresults = props =>{
+    return(
+        <div className="search-books-results">
+            <ol className="books-grid">
+                <Book />
+            </ol>
+        </div>
+    );
+};
 
 export default BooksApp;
